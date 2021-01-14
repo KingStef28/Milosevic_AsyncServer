5e7fbb8525414c85ee7133aeacf0322f4c93b029
Stefan Milosevic <smilosevic86@outlook.it>
2021-01-14 19:14:32 +0100

    Aggiungere i file di progetto.

---
+++ Milosevic_AsyncServer/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Milosevic_AsyncSocketLib;
+
+namespace Milosevic_AsyncServer
+{
+    /// <summary>
+    /// Logica di interazione per MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        AsyncSocketServer mServer;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            mServer = new AsyncSocketServer();
+        }
+
+        private void btn_ascolto_Click(object sender, RoutedEventArgs e)
+        {
+            mServer.InAscolto();
+        }
+    }
+}
